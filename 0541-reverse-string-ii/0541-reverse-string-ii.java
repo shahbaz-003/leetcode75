394c9db96ff266f09a2c75c99de0f26f9281df8d
shahbaz-003 <shahbazmohammed004@gmail.com>
2026-09-03 11:34:08 +0530

    Time: 1 ms (95.91%), Space: 44.8 MB (44.68%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String reverseStr(String s, int k) {
+
+        char[] ch = s.toCharArray();
+
+        for (int i = 0; i < ch.length; i += 2 * k) {
+
+            int left = i;
+            int right = Math.min(i + k - 1, ch.length - 1);
+
+            while (left < right) {
+
+                char temp = ch[left];
+                ch[left] = ch[right];
+                ch[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        return new String(ch);
+    }
+}
