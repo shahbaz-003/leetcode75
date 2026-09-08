649950a0fe575b1e6d98a92484f46c93b37c1f7d
shahbaz-003 <shahbazmohammed004@gmail.com>
2026-09-08 16:00:03 +0530

    Time: 1 ms (99.95%), Space: 46.4 MB (68.13%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        int[] count = new int[26];
+
+        // Count characters in magazine
+        for (char c : magazine.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        // Use characters for ransomNote
+        for (char c : ransomNote.toCharArray()) {
+            count[c - 'a']--;
+
+            if (count[c - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
